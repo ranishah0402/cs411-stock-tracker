ff5375f6cba04c9d09a9afbbb82cda8846938af3
ranishah0402 <ranishah@bu.edu>
2022-04-11 19:43:39 -0400

    did changes get uploaded

---
+++ cs411StockTracker/stock-front-end/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import React from 'react';
+import Stock from './Stock';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Stock></Stock>
+    </div>
+  );
+}
+
+export default App;
---
+++ cs411StockTracker/stock-front-end/src/Stock.js
@@ -0,0 +1,11 @@
+import React from 'react';
+class Stock extends React.Component{
+    render(){
+        return (
+            <div>
+                <h1>Stock Market</h1>
+            </div>
+        )
+    }
+}
+export default Stock
