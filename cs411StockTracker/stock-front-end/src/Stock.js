2679a926cc7a06b2def5de9fc94dd272e1351aac
ranishah0402 <ranishah@bu.edu>
2022-04-11 22:03:35 -0400

    stock might show up on page for amazon

--- cs411StockTracker/stock-front-end/src/Stock.js
+++ cs411StockTracker/stock-front-end/src/Stock.js
@@ -1,9 +1,71 @@
 import React from 'react';
+import Plot from 'react-plotly.js';
 class Stock extends React.Component{
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            stockChartXValues: [],
+            stockChartYValues: []
+
+        }
+    }
+
+    componentDidMount(){
+        this.getStock();
+    }
+
+    getStock(){
+        const POINTER_TO_THIS = this;
+        const API_KEY = 'IOJR3FGTVTYC4D2A';
+        let StockSymbol = 'AMZN'; 
+        let API_Call = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=${StockSymbol}&outputsize=compact&apikey=${API_KEY}';
+        let stockChartXValuesFunction = [];
+        let stockChartYValuesFunction = [];
+
+        fetch(API_Call)
+            .then(
+                function(response){
+                    return response.json();
+                }
+            )
+            .then(
+                function(data){
+                    console.log(data);
+
+                    for (var key in data['Time Series (Daily)']){
+                        stockChartXValuesFunction.push(key);
+                        stockChartYValuesFunction.push(data['Time Series (Daily)'][key]['1. open']);
+                    }
+
+                    POINTER_TO_THIS.setState({
+                        stockChartXValues: stockChartXValuesFunction,
+                        stockChartYValues: stockChartYValuesFunction
+                    });
+                }
+            )
+
+
+    }
+
     render(){
         return (
             <div>
                 <h1>Stock Market</h1>
+                {/*<p>x-values: {this.state.stockChartXValues}</p>
+                <p>y-values: {this.state.stockChartYValues}</p>*/}
+                <Plot
+                    data={[
+                    {
+                        x: this.stockChartXValues,
+                        y: this.stockChartYValues,
+                        type: 'scatter',
+                        mode: 'lines+markers',
+                        marker: {color: 'red'},
+                    }
+                    ]}
+                    layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+                />
             </div>
         )
     }
