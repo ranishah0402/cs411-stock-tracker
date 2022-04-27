708f16eae25647af35b2227b408e3adf62748b5d
Anirudhks64 <52440715+Anirudhks64@users.noreply.github.com>
2022-04-27 16:03:15 -0400

    Updated login feature

--- cs411StockTracker/stock-front-end/stock-front-end/src/pages/login.js
+++ cs411StockTracker/stock-front-end/stock-front-end/src/pages/login.js
@@ -1,13 +1,81 @@
-import React from 'react';
-import Login from './LoginForm';
+import {useRef, useState, useEffect} from 'react';
+import React  from 'react';
+
+const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState('');
+  const[pwd, setPwd] = useState('');
+  const[errMsg, setErrMsg] = useState('');
+  const[success, setSuccess] = useState(false);
+
   
-const LogInPage = () => {
+  useEffect(() => {
+    userRef.current.focus();
+  }, [])
+
+
+  useEffect(() => {
+    setErrMsg('');
+  }, [user, pwd])
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user, pwd);
+    setUser('');
+    setPwd('');
+    setSuccess(true);
+  }
+
   return (
-    <div>
-      <h1> Log in page </h1>
-      <Login />
-    </div>
-  );
-};
-  
-export default LogInPage;
+    <>
+    {success? (
+      <section>
+        <h1>You are logged in!</h1>
+        <br />
+        <p>
+          <a href="#"> Go to Home</a>
+          </p>
+      </section>
+    ) : (
+
+    <section>
+      <p ref={errRef} className={errMsg ? "errmsg" :
+    "offscreen"} aria-live="assertive">{errMsg}</p>
+    <h1>Sign In</h1>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='username'>Username:</label>
+      <input type="text"
+      id="username"
+      ref={userRef}
+      autoComplete="off"
+      onChange={(e) => setUser(e.target.value)}
+      value={user}
+      required
+      />
+      <label htmlFor='password'>Password:</label>
+      <input type="password"
+      id="password"
+      onChange={(e) => setPwd(e.target.value)}
+      value={pwd}
+      required
+      />
+
+    <button>Sign In</button>
+    </form>
+    <p>
+      Need an Account?<br />
+      <span className="line">
+        {/* react router link*/}
+        <a href='#'>Sign Up</a>
+        </span>
+        </p>
+
+
+    </section>
+    )}</>
+  )
+}
+
+export default Login
